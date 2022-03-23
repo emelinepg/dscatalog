40b2e1e8ed53c251f65b841371950f205a40a755
emelinepg <emelinedev84@gmail.com>
2022-03-23 15:28:28 -0300

    Database seeding

--- src/main/java/com/devnoir/dscatalog/services/CategoryService.java
+++ src/main/java/com/devnoir/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devnoir.dscatalog.entities.Category;
 import com.devnoir.dscatalog.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return categoryRepository.findAll();
 	}
