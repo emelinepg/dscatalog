ce234862f4ab216129f345bedce6b303b75b9403
emelinepg <emelinedev84@gmail.com>
2022-03-25 22:10:57 -0300

    Update category

--- src/main/java/com/devnoir/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devnoir/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.devnoir.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devnoir.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), "Resource not found", e.getMessage(), request.getRequestURI()));
 	}
 }
--- src/main/java/com/devnoir/dscatalog/services/CategoryService.java
+++ src/main/java/com/devnoir/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devnoir.dscatalog.dto.CategoryDTO;
 import com.devnoir.dscatalog.entities.Category;
 import com.devnoir.dscatalog.repositories.CategoryRepository;
-import com.devnoir.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devnoir.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -35,21 +37,31 @@ public class CategoryService {
 	
 	@Transactional(readOnly = true) 
 	public CategoryDTO findById(Long id) {
-		Optional<Category> cat = repository.findById(id);
-		Category entity = cat.orElseThrow(() -> new EntityNotFoundException("Entity not found")); 
-		return new CategoryDTO(entity);
+		Optional<Category> opt = repository.findById(id);
+		Category cat = opt.orElseThrow(() -> new ResourceNotFoundException("Entity not found")); 
+		return new CategoryDTO(cat);
 		
 		// outra maneira de fazer (fiz e deu certo):
 		// return new CategoryDTO(cat.get().getId(), cat.get().getName());
 	}
 	
 	@Transactional
-	public CategoryDTO insert(CategoryDTO catDTO) {
+	public CategoryDTO insert(CategoryDTO dto) {
 		Category cat = new Category();
-		cat.setName(catDTO.getName());
+		cat.setName(dto.getName());
 		cat = repository.save(cat);
 		return new CategoryDTO(cat);
 	}
 	
-	
+	@Transactional
+	public CategoryDTO update(CategoryDTO dto, Long id) {
+		try {
+			Category cat = repository.getById(id);
+			cat.setName(dto.getName());
+			cat = repository.save(cat);
+			return new CategoryDTO(cat);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
 }
