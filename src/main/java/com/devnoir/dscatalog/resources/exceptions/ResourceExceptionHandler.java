6306757f198bcbe68ce741230bd9503d6ec7d92a
emelinepg <emelinedev84@gmail.com>
2022-03-26 00:26:36 -0300

    Delete category

--- src/main/java/com/devnoir/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devnoir/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devnoir.dscatalog.services.exceptions.DatabaseException;
 import com.devnoir.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -16,6 +17,13 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), "Resource not found", e.getMessage(), request.getRequestURI()));
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		return ResponseEntity.status(status).body(new StandardError(Instant.now(), status.value(), "Resource not found", e.getMessage(), request.getRequestURI()));
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		return ResponseEntity.status(status).body(new StandardError(Instant.now(), status.value(), "Database exception", e.getMessage(), request.getRequestURI()));
 	}
 }
--- src/main/java/com/devnoir/dscatalog/services/CategoryService.java
+++ src/main/java/com/devnoir/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devnoir.dscatalog.dto.CategoryDTO;
 import com.devnoir.dscatalog.entities.Category;
 import com.devnoir.dscatalog.repositories.CategoryRepository;
+import com.devnoir.dscatalog.services.exceptions.DatabaseException;
 import com.devnoir.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -62,6 +65,16 @@ public class CategoryService {
 			return new CategoryDTO(cat);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
