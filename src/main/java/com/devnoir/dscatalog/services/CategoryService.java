369e1bade0f50e80c9976b46921e5645a64cc7c0
emelinepg <emelinedev84@gmail.com>
2022-03-29 10:30:07 -0300

    Pagination

--- src/main/java/com/devnoir/dscatalog/services/CategoryService.java
+++ src/main/java/com/devnoir/dscatalog/services/CategoryService.java
@@ -1,14 +1,14 @@
 package com.devnoir.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,18 +24,24 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	@Transactional(readOnly = true)
+	/*@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		
-		/*List<CategoryDTO> categories = new ArrayList<>();
-		for (Category x : list) {
-			CategoryDTO dto = new CategoryDTO(x);
-			categories.add(dto);
-		}*/
+		//List<CategoryDTO> categories = new ArrayList<>();
+		//for (Category x : list) {
+		//	CategoryDTO dto = new CategoryDTO(x);
+		//	categories.add(dto);
+		//}
 		
 		// é possível fazer o mesmo com uma expressão lambda
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}*/
+
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 	
 	@Transactional(readOnly = true) 
@@ -77,4 +83,6 @@ public class CategoryService {
 			throw new DatabaseException("Integrity violation");
 		}
 	}
+
+
 }
