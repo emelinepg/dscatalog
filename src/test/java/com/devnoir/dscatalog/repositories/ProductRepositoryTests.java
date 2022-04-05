f63865eefe0baf265e7c715338de165bf03cf882
emelinepg <emelinedev84@gmail.com>
2022-04-05 00:18:59 -0300

    Delete and save tests, BeforeEach

--- src/test/java/com/devnoir/dscatalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/devnoir/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,11 +3,14 @@ package com.devnoir.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devnoir.dscatalog.entities.Product;
+import com.devnoir.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -15,12 +18,42 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
+	private long existingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 100L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		
+		product = repository.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+	}
+	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		repository.deleteById(1L);
+		repository.deleteById(existingId);
 		
-		Optional<Product> result = repository.findById(1L);
+		Optional<Product> result = repository.findById(existingId);
 		
 		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);	
+		});
+		
 	}
 }
