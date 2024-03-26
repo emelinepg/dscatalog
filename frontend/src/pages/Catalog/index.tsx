2a10aa5df0d3ba28fcc501a1c94139ea283acec9
emelinepg <emelinedev84@gmail.com>
2024-03-26 08:02:11 -0300

    Authorized request

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import { useEffect, useState } from 'react';
 import { SpringPage } from 'types/vendor/spring';
-import { BASE_URL } from 'utils/requests';
-import axios, { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'utils/requests';
+import { AxiosRequestConfig } from 'axios';
 import Loader from './Loader';
 
 const Catalog = () => {
@@ -18,7 +18,6 @@ const Catalog = () => {
     const params: AxiosRequestConfig = {
       method: 'GET',
       url: "/products",
-      baseURL: BASE_URL,
       params: {
         page: 0,
         size: 12,
@@ -26,7 +25,7 @@ const Catalog = () => {
     };
 
     setIsLoading(true);
-    axios(params)
+    requestBackend(params)
       .then((response) => {
         setPage(response.data);
       })
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,14 +1,14 @@
-import axios from "axios";
-import qs from "qs";
+import axios, { AxiosRequestConfig } from 'axios';
+import qs from 'qs';
 
 type LoginResponse = {
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-    scope: string;
-    userFirstName: string;
-    userId: number
-}
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
 
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
@@ -34,16 +34,32 @@ export const requestBackendLogin = (loginData: LoginData) => {
     grant_type: 'password',
   });
 
-  return axios({method: 'POST', baseURL: BASE_URL, url:'/oauth/token', data, headers})
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
 };
 
-export const saveAuthData = (obj : LoginResponse) => {
-    localStorage.setItem(tokenKey, JSON.stringify(obj));
-}
+export const requestBackend = (config: AxiosRequestConfig) => {
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: 'Bearer' + getAuthData().access_token,
+      }
+    : config.headers;
+
+  return axios({ ...config, baseURL: BASE_URL, headers });
+};
+
+export const saveAuthData = (obj: LoginResponse) => {
+  localStorage.setItem(tokenKey, JSON.stringify(obj));
+};
 
 export const getAuthData = () => {
-    const str = localStorage.getItem(tokenKey) ?? "";
+  const str = localStorage.getItem(tokenKey) ?? '';
 
-    return JSON.parse(str) as LoginResponse;
-
-}
+  return JSON.parse(str) as LoginResponse;
+};
