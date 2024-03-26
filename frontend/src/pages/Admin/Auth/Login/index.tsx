68eca5e18de3424f912ed659b17fb1837fcf0828
emelinepg <emelinedev84@gmail.com>
2024-03-26 08:08:23 -0300

    Redirecting programmatically

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import ButtonIcon from 'components/ButtonIcon'
+import { Link, useHistory } from 'react-router-dom';
+import ButtonIcon from 'components/ButtonIcon';
 import './styles.css';
 import { useForm } from 'react-hook-form';
 import { getAuthData, requestBackendLogin, saveAuthData } from 'utils/requests';
@@ -12,58 +12,75 @@ type FormData = {
 
 const Login = () => {
   const [hasError, setHasError] = useState(false);
-  
-    const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const history = useHistory();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
-    .then(response => {
+      .then((response) => {
         saveAuthData(response.data);
         const token = getAuthData().access_token;
         console.log('Token gerado: ' + token);
         setHasError(false);
         console.log('Sucesso', response);
-    })
-    .catch(error => {
+        history.push('/admin');
+      })
+      .catch((error) => {
         setHasError(true);
         console.log('Erro', error);
-    })
+      });
   };
 
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      { hasError && (<div className="alert alert-danger">
-        Ocorreu um erro ao tentar efetuar o login
-      </div>)}
+      {hasError && (
+        <div className="alert alert-danger">
+          Ocorreu um erro ao tentar efetuar o login
+        </div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
             {...register('username', {
-                required: 'Campo obrigatório',
-                pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: 'Email inválido'
-                }
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido',
+              },
             })}
             type="text"
-            className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
+            className={`form-control base-input ${
+              errors.username ? 'is-invalid' : ''
+            }`}
             placeholder="Email"
             name="username"
           />
-          <div className="invalid-feedback d-block">{errors.username?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
             {...register('password', {
-                required: 'Campo obrigatório'
+              required: 'Campo obrigatório',
             })}
             type="password"
-            className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
+            className={`form-control base-input ${
+              errors.password ? 'is-invalid' : ''
+            }`}
             placeholder="Password"
             name="password"
           />
-          <div className="invalid-feedback d-block">{errors.password?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
