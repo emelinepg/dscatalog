a3a52233148fb9d4ac53716cb4f10ab2aa50cd66
emelinepg <emelinedev84@gmail.com>
2024-03-30 09:47:51 -0300

    Form buttons styling

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -9,17 +9,27 @@ const Form = () => {
       <form action="">
         <div className="row">
           <div className="col-lg-6">
-            <input type="text" className="form-control base-input"/>
-            <input type="text" className="form-control base-input" />
-            <input type="text" className="form-control base-input"/>
+            <div className="product-crud-input">
+              <input type="text" className="form-control base-input" />
+            </div>
+            <div className="product-crud-input">
+              <input type="text" className="form-control base-input" />
+            </div>
+            <div className="product-crud-input">
+              <input type="text" className="form-control base-input" />
+            </div>
           </div>
-          <div className="col-lg-6">
-            <textarea name="" rows={10} className="form-control base-input"></textarea>
+          <div className="col-lg-6 product-crud-input">
+            <textarea
+              name=""
+              rows={10}
+              className="form-control base-input h-auto"
+            ></textarea>
           </div>
         </div>
-        <div>
-            <button className="btn btn-outline-danger">CANCELAR</button>
-            <button className="btn btn-primary">SALVAR</button>
+        <div className="product-crud-form-buttons-container">
+          <button className="btn btn-outline-danger product-crud-form-button">CANCELAR</button>
+          <button className="btn btn-primary text-white product-crud-form-button">SALVAR</button>
         </div>
       </form>
     </div>
