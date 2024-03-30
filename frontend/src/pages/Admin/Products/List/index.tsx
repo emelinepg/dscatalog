bc9f74f0edfb148e9f16ce2c0d9ff1dbed35d721
emelinepg <emelinedev84@gmail.com>
2024-03-30 11:36:52 -0300

    CRUD - Delete, observer pattern

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -10,6 +10,10 @@ import { requestBackend } from 'utils/requests';
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
   useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
@@ -22,7 +26,7 @@ const List = () => {
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  }, []);
+  };
 
   return (
     <>
@@ -37,7 +41,7 @@ const List = () => {
       <div className="row">
         {page?.content.map((product) => (
           <div className="col-sm-6 col-md-12" key={product.id}>
-            <ProductCrudCard product={product} />
+            <ProductCrudCard product={product} onDelete={() => getProducts()} />
           </div>
         ))}
       </div>
