6b22c1824b5c81231503745011f7df4966476ac8
emelinepg <emelinedev84@gmail.com>
2024-03-30 16:19:31 -0300

    ProductFilter - React hook form

--- frontend/src/components/ProductFilter/index.tsx
+++ frontend/src/components/ProductFilter/index.tsx
@@ -1,30 +1,85 @@
+import { Category } from 'types/category';
 import './styles.css';
-import {ReactComponent as SearchIcon} from 'assets/images/search.svg'
+import { ReactComponent as SearchIcon } from 'assets/images/search.svg';
+import { Controller, useForm } from 'react-hook-form';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'utils/requests';
+import Select from 'react-select';
+import { useEffect, useState } from 'react';
+
+type ProductFilterData = {
+  name: string;
+  category: Category;
+};
 
 const ProductFilter = () => {
-    
-    return (
-        <div className="base-card product-filter-container">
-            <form action="" className="product-filter-form">
-                <div className="product-filter-name-container">
-                    <input 
-                    type="text" 
-                    className="form-control"
-                    placeholder="Nome do produto"
-                    />
-                    <SearchIcon/>
-                </div>
-                <div className="product-filter-options-container">
-                    <div className="product-filter-category-container">
-                    <select name="" id="">
-                        <option value="">Livros</option>
-                    </select>
-                    </div>
-                    <button className="btn btn-outline-secondary">LIMPAR</button>
-                </div>
-            </form>
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    requestBackend({ url: '/categories' }).then((response) => {
+      setSelectCategories(response.data.content);
+    });
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    control,
+  } = useForm<ProductFilterData>();
+
+  const onSubmit = (formData: ProductFilterData) => {
+    console.log('ENVIOU', formData);
+    /*     const data = {...formData, price: String(formData.price).replace(',','.')}
+
+    const config: AxiosRequestConfig = {
+      method: isEditing ? 'PUT' : 'POST',
+      url: isEditing ? `/products/${productId}` : '/products',
+      data,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      history.push('/admin/products');
+    }); */
+  };
+
+  return (
+    <div className="base-card product-filter-container">
+      <form onSubmit={handleSubmit(onSubmit)} className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input
+            {...register('name')}
+            type="text"
+            className="form-control"
+            placeholder="Nome do produto"
+            name="name"
+          />
+          <button>
+            <SearchIcon />
+          </button>
         </div>
-    )
-}
+        <div className="product-filter-options-container">
+          <div className="product-filter-category-container">
+            <Controller
+              name="category"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={selectCategories}
+                  isClearable
+                  classNamePrefix="product-crud-form-select"
+                  getOptionLabel={(category: Category) => category.name}
+                  getOptionValue={(category: Category) => String(category.id)}
+                />
+              )}
+            />
+          </div>
+          <button className="btn btn-outline-secondary">LIMPAR</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default ProductFilter;
