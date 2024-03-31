c61a7f52431d6f5b9e3f932221aac5e97fce6b4f
emelinepg <emelinedev84@gmail.com>
2024-03-31 16:35:57 -0300

    msw mock server, fixtures

--- frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,7 +2,11 @@ import { render, screen, waitFor } from "@testing-library/react";
 import Catalog from "..";
 import { Router } from 'react-router-dom';
 import history from "utils/history";
+import { server } from './fixtures';
 
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('Should render Catalog with products', async() => {
     //ARRANGE
